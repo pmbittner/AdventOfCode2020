c8949761be5a462dbb19750775e466b5bc56e49e
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-06-21 16:36:53 +0200

    file type

--- src/Door.hs
+++ src/Door.hs
@@ -1,7 +1,8 @@
 ﻿module Door where
 
+type File = String
 data Door = Door {
     no :: Int, -- The number of the Door
-    files :: [String], -- List of files that should be processed
-    runDoor :: String -> IO() -- functions to run this puzzle's solution on the given text
+    files :: [File], -- List of files that should be processed
+    runDoor :: File -> IO() -- functions to run this puzzle's solution on the given text
 }
